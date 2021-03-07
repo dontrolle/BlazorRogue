f130b005ed18f79b2821d7b651331017ab0818bf
Troels Damgaard <dontrolle@gmail.com>
2021-03-07 23:28:49 +0100

    Character on StaticDecorativeObject

--- GameObjects/StaticDecorativeObject.cs
+++ GameObjects/StaticDecorativeObject.cs
@@ -11,17 +11,21 @@ namespace BlazorRogue.GameObjects
         private readonly string png;
         private readonly string? infoText;
         private readonly int offset;
+        private readonly string character;
+        private readonly string characterColor;
 
-        public StaticDecorativeObject(int x, int y, string name, string png, string? infoText = null, int offset = 0) : base(x, y, name)
+        public StaticDecorativeObject(int x, int y, string name, string png, string? infoText = null, int offset = 0, string character = "", string characterColor = "purple") : base(x, y, name)
         {
             this.png = png;
             this.infoText = infoText;
             this.offset = offset;
+            this.character = character;
+            this.characterColor = characterColor;
         }
 
         public override void Render(Map map)
         {
-            map.Decorations[x, y].Add(new Decoration(this, png) { Offset = offset });
+            map.Decorations[x, y].Add(new Decoration(this, png) { Offset = offset, Character = character, CharacterColor = characterColor });
         }
     }
 }
