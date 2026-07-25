54a6b90efc736b25abe938187177f3bf14699f3c
Troels Damgaard <troels@datasolvr.com>
2026-07-25 13:00:09 +0200

    refactor: improve FirstLetterToUpperCase method and update documentation (#15)

--- UtilityMethods.cs
+++ UtilityMethods.cs
@@ -8,16 +8,14 @@ namespace BlazorRogue
   public static class UtilityMethods
   {
     /// <summary>
-    /// Returns the input string with the first character converted to uppercase, or mutates any nulls passed into string.Empty
+    /// Returns a string equal to the input string with the first character converted to uppercase, or string.Empty if null or the empty string is passed.
     /// </summary>
     public static string FirstLetterToUpperCase(this string s)
     {
       if (string.IsNullOrEmpty(s))
         return string.Empty;
 
-      char[] a = s.ToCharArray();
-      a[0] = char.ToUpper(a[0]);
-      return new string(a);
+      return String.Concat(s[..1].ToUpper(), s[1..].AsSpan());
     }
   }
 }
