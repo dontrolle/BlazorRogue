d05ba2f58751584ac9d11b61cf22e0a689893b29
Troels Damgaard <troels@tedconsulting.dk>
2026-08-11 20:21:08 +0200

    feat: split dungeon gen in three (#62)

---
+++ World/CaveGenerator.cs
@@ -0,0 +1,192 @@
+using System;
+using System.Linq;
+using BlazorRogue.Entities;
+using BlazorRogue.GameObjects;
+
+namespace BlazorRogue.World;
+
+class CaveGenerator(int width, int height, Game game)
+    : DungeonGeneratorBase(
+        width,
+        height,
+        game,
+        DungeonGeneratorBase.SelectRandom(game.Configuration.CaveWallSets)
+    )
+{
+    protected override Tuple<int, int> CreateLayout() => CreateCave();
+
+    Tuple<int, int> CreateCave()
+    {
+        int initWallPercentageChance = 40;
+
+        bool[,] genmap = new bool[map.Width, map.Height];
+
+        void InitFill(int x, int y) =>
+            genmap[x, y] = random.Next(1, 101) < initWallPercentageChance;
+
+        map.ForEachTile(InitFill);
+
+        bool[,]? newmap = null;
+        void Generation1Fill(int x, int y) =>
+            newmap[x, y] =
+                SurroundingWallNumberWithinN(genmap, x, y, 1) >= 5
+                || SurroundingWallNumberWithinN(genmap, x, y, 2) <= 1;
+
+        for (int i = 0; i < 4; i++)
+        {
+            newmap = new bool[map.Width, map.Height];
+            map.ForEachTile(Generation1Fill);
+            genmap = newmap;
+        }
+
+        void Generation2Fill(int x, int y) =>
+            newmap[x, y] = SurroundingWallNumberWithinN(genmap, x, y, 1) >= 5;
+
+        for (int i = 0; i < 3; i++)
+        {
+            newmap = new bool[map.Width, map.Height];
+            map.ForEachTile(Generation2Fill);
+            genmap = newmap;
+        }
+
+        // fill border area
+        for (int x = 0; x < map.Width; x++)
+        {
+            for (int y = 0; y < map.Height; y++)
+            {
+                if (x == 0 || x == map.Width - 1 || y == 0 || y == map.Height - 1)
+                    genmap[x, y] = true;
+            }
+        }
+
+        return FinalizeCaveGen(genmap);
+    }
+
+    Tuple<int, int> FinalizeCaveGen(bool[,] genmap)
+    {
+        // TODO: UF
+        string caveGenTileSet = "crusted_grey";
+        var floorset =
+            configuration.SpecialFloorSets.FirstOrDefault(t => t.Id == caveGenTileSet)
+            ?? throw new InvalidOperationException(
+                "Missing tileset " + caveGenTileSet + " in read configuration."
+            );
+        int[] floorIndexes = [1, 2, 3, 4];
+        FillMap(genmap, floorset);
+
+        return GetRandomUnblockedMapTile();
+    }
+
+    void FillMap(bool[,] genmap, TileSet floorset) =>
+        map.ForEachTile(
+            (x, y) =>
+            {
+                if (genmap[x, y])
+                    PlaceWall(x, y);
+                else
+                    PlaceFloor(x, y, floorset);
+            }
+        );
+
+    int SurroundingWallNumberWithinN(bool[,] genmap, int x, int y, int distance)
+    {
+        int noOfWalls = 0;
+
+        for (int dx = -distance; dx < distance + 1; dx++)
+        {
+            for (int dy = -distance; dy < distance + 1; dy++)
+            {
+                // consider outside of map as walls
+                // TODO: check map - slightly wrong...
+                if (x + dx < 0 || x + dx > map.Width - 1 || y + dy < 0 || y + dy > map.Height - 1)
+                {
+                    noOfWalls++;
+                }
+                else if (genmap[x + dx, y + dy])
+                {
+                    noOfWalls++;
+                }
+            }
+        }
+
+        return noOfWalls;
+    }
+
+    protected override void OnNorthHalfWallPlaced(int x, int y)
+    {
+        // add cave_edge_1 and 2 to halfwall-tiles offset to the left and right respectively
+        if (
+            x > 0
+            && (
+                map.Tiles[x - 1, y].TileType == TileType.Floor
+                || map.Tiles[x - 1, y].TileType == TileType.Black
+            )
+        )
+        {
+            map.AddGameObject(new CaveEdge(x, y, 1, -1, -1));
+        }
+
+        if (
+            x < map.Width - 1
+            && (
+                map.Tiles[x + 1, y].TileType == TileType.Floor
+                || map.Tiles[x + 1, y].TileType == TileType.Black
+            )
+        )
+        {
+            map.AddGameObject(new CaveEdge(x, y, 2, -1, 1));
+        }
+    }
+
+    protected override void OnSouthWallFrontPlaced(int x, int y)
+    {
+        // add cave_edge_5 and 6 to wall tiles with front offset to the left and right respectively
+        if (
+            x > 0
+            && (
+                map.Tiles[x - 1, y].TileType == TileType.Floor
+                || map.Tiles[x - 1, y].TileType == TileType.Black
+            )
+        )
+        {
+            map.AddGameObject(new CaveEdge(x, y, 5, 0, -1));
+        }
+
+        if (
+            x < map.Width - 1
+            && (
+                map.Tiles[x + 1, y].TileType == TileType.Floor
+                || map.Tiles[x + 1, y].TileType == TileType.Black
+            )
+        )
+        {
+            map.AddGameObject(new CaveEdge(x, y, 6, 0, 1));
+        }
+    }
+
+    protected override void OnWallTileVisited(int x, int y)
+    {
+        // add cave_edge_3 and 4 to normal wall tiles offset to the left and right respectively
+        if (
+            x > 0
+            && (
+                map.Tiles[x - 1, y].TileType == TileType.Floor
+                || map.Tiles[x - 1, y].TileType == TileType.Black
+            )
+        )
+        {
+            map.AddGameObject(new CaveEdge(x, y, 3, 0, -1));
+        }
+
+        if (
+            x < map.Width - 1
+            && (
+                map.Tiles[x + 1, y].TileType == TileType.Floor
+                || map.Tiles[x + 1, y].TileType == TileType.Black
+            )
+        )
+        {
+            map.AddGameObject(new CaveEdge(x, y, 4, 0, 1));
+        }
+    }
+}
---
+++ World/DungeonGeneratorBase.cs
@@ -0,0 +1,439 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BlazorRogue.AI;
+using BlazorRogue.Components;
+using BlazorRogue.Entities;
+using BlazorRogue.GameObjects;
+
+namespace BlazorRogue.World;
+
+abstract class DungeonGeneratorBase(int width, int height, Game game, TileSet wallSet)
+    : IDungeonGenerator
+{
+    protected readonly Map map = new(width, height, wallSet, game);
+    protected readonly Configuration configuration = game.Configuration;
+    protected readonly Random random = new();
+
+    // Decorations
+    protected const double PercentageChanceOfBones = 0.05;
+    protected readonly double percentageChanceOfTables = 0.06;
+    protected readonly double percentageChanceOfAltars = 0.04;
+    protected readonly double percentageChanceOfSpiderWebInCorner = 0.25;
+    protected readonly double percentageChanceOfTorch = 0.25;
+    protected readonly double percentageChanceOfChests = 0.02;
+
+    // TODO: UF
+    protected readonly string[] doorTypes = ["metal", "stone", "wood", "ruin"];
+
+    protected readonly List<Tuple<int, int>> candidateDoors = [];
+
+    // Picks a random element ahead of the base constructor running, e.g. for choosing a subclass's
+    // wall set from a constructor initializer - at that point the instance `random` field (and any
+    // other instance state) hasn't been initialized yet, so it can't be used.
+#pragma warning disable CA1851 // Possible multiple enumerations of 'IEnumerable' collection
+    protected static T SelectRandom<T>(IEnumerable<T> elements) =>
+        elements.ElementAt(Random.Shared.Next(elements.Count()));
+#pragma warning restore CA1851 // Possible multiple enumerations of 'IEnumerable' collection
+
+    public Map GenerateMap()
+    {
+        var playerPos = CreateLayout();
+
+        AddDoors();
+        AddPostGenerationDecorations();
+
+        // Add Player
+        var heroType = GetRandomElement(configuration.HeroTypes).Value;
+        var player = new Moveable(playerPos, null, heroType, new InventoryComponent());
+
+        map.AddPlayer(player);
+
+        // Add monsters
+        int noOfRandomMonsters = 8;
+
+        for (int i = 0; i < noOfRandomMonsters; i++)
+        {
+            var pos = GetRandomUnblockedMapTile();
+            var monsterType = GetRandomElement(configuration.MonsterTypes).Value;
+            var monster = new Moveable(pos, new SimpleAIComponent(map), monsterType);
+            map.AddMonster(monster);
+        }
+
+        var extraPos = GetRandomUnblockedMapTile();
+        var extraGoblin = new Moveable(
+            extraPos,
+            new SimpleAIComponent(map),
+            configuration.MonsterTypes["goblin"]
+        );
+        map.AddMonster(extraGoblin);
+
+        // initialize various maps and so on in Map (it there a better place to do this?)
+        map.PostGenInitalize();
+
+        return map;
+    }
+
+    protected abstract Tuple<int, int> CreateLayout();
+
+    protected void AddDoors()
+    {
+        foreach (var candidateDoor in candidateDoors)
+        {
+            int x = candidateDoor.Item1;
+            int y = candidateDoor.Item2;
+            if (map.Tiles[x, y].TileType == TileType.Floor)
+            {
+                // Check if horizontal makes sense
+                if (
+                    x > 1
+                    && x < map.Width - 1
+                    && map.Tiles[x - 1, y].TileType == TileType.Wall
+                    && map.Tiles[x + 1, y].TileType == TileType.Wall
+                )
+                {
+                    if (!MapTileContainsDoor(x, y))
+                    {
+                        map.AddGameObject(
+                            new Door(
+                                x,
+                                y,
+                                GetRandomElement(doorTypes),
+                                random.Next(1, 4),
+                                Orientation.Horizontal,
+                                GetRandomBool()
+                            )
+                        );
+                    }
+                }
+                // Check if vertical makes sense
+                else if (
+                    y > 1
+                    && y < map.Height - 1
+                    && map.Tiles[x, y - 1].TileType == TileType.Wall
+                    && map.Tiles[x, y + 1].TileType == TileType.Wall
+                )
+                {
+                    if (!MapTileContainsDoor(x, y))
+                    {
+                        map.AddGameObject(
+                            new Door(
+                                x,
+                                y,
+                                GetRandomElement(doorTypes),
+                                random.Next(1, 4),
+                                Orientation.Vertical,
+                                GetRandomBool()
+                            )
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    protected Tuple<int, int> GetRandomUnblockedMapTile()
+    {
+        int maxSearch = 200;
+        for (int i = 0; i < maxSearch; i++)
+        {
+            int x = random.Next(0, map.Width);
+            int y = random.Next(0, map.Height);
+
+            if (!map.IsBlocked(x, y))
+                return Tuple.Create(x, y);
+        }
+        throw new InvalidOperationException(
+            $"Couldn't find an unblocked tile on map in {maxSearch} tries!"
+        );
+    }
+
+    protected void AddPostGenerationDecorations()
+    {
+        for (int x = 0; x < map.Width; x++)
+        {
+            for (int y = 0; y < map.Height; y++)
+            {
+                AddPostGenWallDecorations(x, y);
+
+                AddPostGenFloorDecorations(x, y);
+            }
+        }
+    }
+
+    protected void AddPostGenFloorDecorations(int x, int y)
+    {
+        if (map.Tiles[x, y].TileType == TileType.Floor)
+        {
+            if (
+                random.NextDouble() < PercentageChanceOfBones
+                && !MapTileContainsDoor(x, y)
+                && !map.IsBlocked(x, y)
+            )
+            {
+                map.AddGameObject(
+                    new StaticDecorativeObject(
+                        x,
+                        y,
+                        configuration.StaticDecorativeObjectTypes["bones"]
+                    )
+                );
+            }
+
+            if (
+                random.NextDouble() < percentageChanceOfTables
+                && !MapTileContainsDoor(x, y)
+                && !map.IsBlocked(x, y)
+            )
+            {
+                if (NumberOfSurroundingBlockingSpots(x, y) < 4)
+                {
+                    map.AddGameObject(
+                        new StaticDecorativeObject(
+                            x,
+                            y,
+                            configuration.StaticDecorativeObjectTypes["table"]
+                        )
+                    );
+                }
+            }
+
+            if (
+                random.NextDouble() < percentageChanceOfAltars
+                && !MapTileContainsDoor(x, y)
+                && !map.IsBlocked(x, y)
+            )
+            {
+                if (NumberOfSurroundingBlockingSpots(x, y) < 4)
+                {
+                    map.AddGameObject(
+                        new StaticDecorativeObject(
+                            x,
+                            y,
+                            configuration.StaticDecorativeObjectTypes["altar_blood"]
+                        )
+                    );
+                }
+            }
+
+            if (
+                random.NextDouble() < percentageChanceOfChests
+                && !MapTileContainsDoor(x, y)
+                && !map.IsBlocked(x, y)
+            )
+            {
+                string chestId = "chest_silver";
+                int gold = random.Next(0, 4);
+                if (random.Next(0, 4) == 0)
+                {
+                    chestId = "chest_gold";
+                    gold += 4;
+                }
+
+                map.AddGameObject(
+                    new Chest(x, y, chestId, new InventoryComponent() { Gold = gold })
+                );
+            }
+
+            // in the following we rely on floors never being placed on the perimeter tiles, else we could do
+            //if(x > 0 && x < map.Width -1 && y > 0 && y < map.Height - 1){ ... }
+            if (random.NextDouble() < percentageChanceOfSpiderWebInCorner)
+            {
+                bool wallAbove = map.Tiles[x, y - 1].TileType == TileType.Wall;
+                bool wallBelow = map.Tiles[x, y + 1].TileType == TileType.Wall;
+                bool wallLeft = map.Tiles[x - 1, y].TileType == TileType.Wall;
+                bool wallRight = map.Tiles[x + 1, y].TileType == TileType.Wall;
+
+                string corner = "";
+                int verticalOffset = 0;
+                if (wallAbove && wallLeft)
+                {
+                    corner = "NW";
+                    verticalOffset = -1;
+                }
+                else if (wallBelow && wallLeft)
+                {
+                    corner = "SW";
+                }
+                else if (wallBelow && wallRight)
+                {
+                    corner = "SE";
+                }
+                else if (wallAbove && wallRight)
+                {
+                    corner = "NE";
+                    verticalOffset = -1;
+                }
+
+                if (!string.IsNullOrEmpty(corner))
+                {
+                    // i.e., we found a suitable spot for a spiderweb
+                    map.AddGameObject(
+                        new StaticDecorativeObject(
+                            x,
+                            y,
+                            configuration.StaticDecorativeObjectTypes["corner_spiderweb"],
+                            corner,
+                            verticalOffset
+                        )
+                    );
+                }
+            }
+        }
+    }
+
+    protected int NumberOfSurroundingBlockingSpots(int x, int y)
+    {
+        int numberOfSurroundingBlockingSpots = 0;
+
+        for (int dx = -1; dx < 2; dx++)
+        {
+            for (int dy = -1; dy < 2; dy++)
+            {
+                if (dx == 0 && dy == 0)
+                {
+                    continue;
+                }
+
+                if (map.IsBlocked(x + dx, y + dy))
+                {
+                    numberOfSurroundingBlockingSpots++;
+                }
+            }
+        }
+
+        return numberOfSurroundingBlockingSpots;
+    }
+
+    protected void AddPostGenWallDecorations(int x, int y)
+    {
+        // Add halfwall decorations on all wall tiles (offset -1) with a floor-tile or a black tile directly above
+        // if tile above has door, select from 1-3, else from tiles 1-6
+        if (y > 0)
+        {
+            if (
+                map.Tiles[x, y].TileType == TileType.Wall
+                && (
+                    map.Tiles[x, y - 1].TileType == TileType.Floor
+                    || map.Tiles[x, y - 1].TileType == TileType.Black
+                )
+            )
+            {
+                int[] halfwallIndexes = map.DungeonWallSet.ImageEdgeNorthIndexes;
+
+                bool restrictToSimplerHalfWall =
+                    MapTileContainsDoor(x, y - 1) || random.Next(0, 4) < 3;
+                if (restrictToSimplerHalfWall)
+                {
+                    halfwallIndexes = map.DungeonWallSet.ImageSimpleEdgeNorthIndexes;
+                }
+
+                map.AddGameObject(new HalfWall(x, y, GetRandomElement(halfwallIndexes)));
+
+                OnNorthHalfWallPlaced(x, y);
+            }
+        }
+
+        // Wall should have front, if there is a floor tile or a black tile below; if tile below has a door, choose 14
+        if (y < map.Height - 1)
+        {
+            if (
+                map.Tiles[x, y].TileType == TileType.Wall
+                && (
+                    map.Tiles[x, y + 1].TileType == TileType.Floor
+                    || map.Tiles[x, y + 1].TileType == TileType.Black
+                )
+            )
+            {
+                int index = GetRandomElementWeighted(
+                    map.DungeonWallSet.ImageSouthEdgeIndexes,
+                    map.DungeonWallSet.ImageSouthEdgeWeights
+                );
+                bool mapTileBelowHasDoor = MapTileContainsDoor(x, y + 1);
+                if (mapTileBelowHasDoor)
+                {
+                    // TODO: UF
+                    index = 14;
+                }
+                map.Tiles[x, y].TileIndex = index;
+
+                // check for adding torch
+                // TODO: UF
+                if (
+                    !mapTileBelowHasDoor
+                    && map.Tiles[x, y + 1].TileType == TileType.Floor
+                    && random.NextDouble() < percentageChanceOfTorch
+                )
+                {
+                    map.AddGameObject(new Torch(x, y));
+                    //map.DebugInfo.Add($"Added torch at ({x},{y}).");
+                }
+
+                OnSouthWallFrontPlaced(x, y);
+            }
+        }
+
+        if (map.Tiles[x, y].TileType == TileType.Wall)
+        {
+            OnWallTileVisited(x, y);
+        }
+    }
+
+    // Hooks for subclass-specific extra wall decorations (e.g. cave edges), called from
+    // AddPostGenWallDecorations above. No-op by default.
+    protected virtual void OnNorthHalfWallPlaced(int x, int y) { }
+
+    protected virtual void OnSouthWallFrontPlaced(int x, int y) { }
+
+    protected virtual void OnWallTileVisited(int x, int y) { }
+
+    protected bool MapTileContainsDoor(int x, int y) =>
+        map.GameObjectByCoord[x, y].Any(go => go is Door);
+
+    protected T GetRandomElement<T>(T[] elements) => elements[random.Next(0, elements.Length)];
+
+#pragma warning disable CA1851 // Possible multiple enumerations of 'IEnumerable' collection
+    protected T GetRandomElement<T>(IEnumerable<T> elements) =>
+        elements.ElementAt(random.Next(0, elements.Count()));
+#pragma warning restore CA1851 // Possible multiple enumerations of 'IEnumerable' collection
+
+    protected T GetRandomElementWeighted<T>(T[] elements, double[] weights)
+    {
+        if (elements.Length != weights.Length)
+            throw new ArgumentException("elements and weigths should be of same length.");
+
+        int i;
+        double r = random.NextDouble() * weights.Sum();
+        for (i = 0; i < weights.Length; i++)
+        {
+            if (r < weights[i])
+            {
+                break;
+            }
+            r -= weights[i];
+        }
+
+        return elements[i];
+    }
+
+    protected bool GetRandomBool() => random.Next(0, 2) == 0;
+
+    protected void PlaceWall(int x, int y)
+    {
+        // TODO: Fix - right now important to clear all properties, else some may remain from earlier floor, e.g.
+        map.Tiles[x, y].TileSet = map.DungeonWallSet;
+        map.Tiles[x, y].TileIndex = GetRandomElementWeighted(
+            map.DungeonWallSet.ImageBaseIndexes,
+            map.DungeonWallSet.ImageBaseWeights
+        );
+        map.Tiles[x, y].Blocking = true;
+    }
+
+    protected void PlaceFloor(int x, int y, TileSet floorSet)
+    {
+        // TODO: Fix - right now important to clear all properties, else some may remain from earlier wall, e.g.
+        map.Tiles[x, y].TileSet = floorSet;
+        map.Tiles[x, y].TileIndex = GetRandomElement(floorSet.ImageBaseIndexes);
+        map.Tiles[x, y].Blocking = false;
+    }
+}
